484ca7f0373ca457ddb1979a1e06f6b3180f3bdb
Xavier <xavier.carrau@alumni.enac.fr>
2021-02-25 10:12:44 +0100

    changement de variables en class Commande

--- src/projet/multimodal/Commande.java
+++ src/projet/multimodal/Commande.java
@@ -6,14 +6,53 @@ import java.awt.Color;
 public class Commande {
     String action, objet;
     int posX, posY;
-    Color couleur;
+    String couleur;
     
     public Commande(){
         action = null;
         objet = null;
         posX= -1;
         posY= -1;
-        couleur = null;
+        couleur = "Black";
     }
-  
+
+    public String getAction() {
+        return action;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public String getObjet() {
+        return objet;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+    public void setObjet(String objet) {
+        this.objet = objet;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
 }
